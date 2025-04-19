3171d2b8e588910de20a274ada36929cccf4d0f8
CubeCat <sanderlapimaa@gmail.com>
2025-04-20 01:38:43 +0300

    fixed cpu lookup fuck you techpowerup

--- commands/general/cpuSearch.js
+++ commands/general/cpuSearch.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 async function scrapeCpuList(query) {
-    const url = `https://www.techpowerup.com/cpu-specs/?ajaxsrch=${encodeURIComponent(query)}`;
+    const url = `https://www.techpowerup.com/gpu-specs/?ajaxsrch=&q${encodeURIComponent(query)}`;
     const response = await axios.get(url, {
         headers: {
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
