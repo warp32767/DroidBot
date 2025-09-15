067f5fd68cceca446bbf0d0d362c4c00ad1437b5
warp32767 <sanderlapimaa@gmail.com>
2025-09-15 19:57:26 +0300

    push and pray 4!!!!

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 const { REST } = require("@discordjs/rest");
 const { Client, Collection, GatewayIntentBits, EmbedBuilder, ActivityType, AttachmentBuilder } = require("discord.js");
-const axios = require('axios');
-const fs = require('node:fs');
+const axios = require("axios");
+const fs = require("node:fs");
 const { loadEvents } = require("./handler/events");
-const { token } = require('./config.json');
-const { version } = require('./package.json');
+const { token } = require("./config.json");
+const { version } = require("./package.json");
 const { loadCommands } = require("./handler/slashCommands");
 const { deployCommands } = require("./handler/deployCommands");
 
-// Import sensor library at the top
 const sensor = require("node-dht-sensor").promises;
 
-// Configure the sensor (type 22 = DHT22, GPIO pin = e.g., 4)
-const sensorType = 22; // 22 = DHT22, 11 = DHT11
-const gpioPin = 4; // Adjust to the GPIO pin you connected the sensor to
+// i vibe coded this part because i dont get paid enough
+const sensorType = 22;
+const gpioPin = 4;
 
-// Function to read from the sensor and update bot activity
+// this maybe does something
 async function updateSensorActivity(client) {
     try {
         const { temperature, humidity } = await sensor.read(sensorType, gpioPin);
-        const activityMessage = `Temp: ${temperature.toFixed(1)}°C, Humidity: ${humidity.toFixed(1)}%`;
+        const activityMessage = `Chassis Temp: ${temperature.toFixed(1)}°C, Humidity: ${humidity.toFixed(1)}%`;
 
-        // Update bot's presence
+        // should work, if it doesn't i'm killing myself
         client.user.setActivity(activityMessage, { type: ActivityType.Watching });
         console.log(`Updated bot activity to: ${activityMessage}`);
     } catch (err) {
@@ -43,7 +42,9 @@ const logger = require("./handler/logger");
 
 // Check for updates on startup
 async function checkForUpdates() {
-    const commitJson = await axios.get('https://api.github.com/repos/warp32767/DroidBot/commits/master');
+    const commitJson = await axios.get(
+        "https://api.github.com/repos/warp32767/DroidBot/commits/master"
+    );
     const commitData = commitJson.data;
     const commit = commitData.sha;
     return commit.slice(0, 7);
@@ -53,21 +54,31 @@ async function checkForUpdates() {
 async function setupEmbed() {
     const remoteCommit = await checkForUpdates();
 
-    let localCommit = await fs.readFileSync('.git/refs/heads/master', 'utf8');
+    let localCommit = await fs.readFileSync(".git/refs/heads/master", "utf8");
     const icon = new AttachmentBuilder(`./images/droid.png`);
     const embed = new EmbedBuilder()
         .setTitle(`DroidBot \`${version}\``)
-        .setColor('#ff0000')
+        .setColor("#ff0000")
         .setThumbnail(`attachment://droid.png`)
         .setFooter({ text: `By warp32767` });
 
     if (remoteCommit === localCommit.slice(0, 7)) {
-        embed.setDescription(`Hello droiders!\n\n**DroidBot is up to date!**\nLocal Version: \`${localCommit.slice(0, 7)}\`\nLatest Version: \`${remoteCommit}\``);
+        embed.setDescription(
+            `Hello droiders!\n\n**DroidBot is up to date!**\nLocal Version: \`${localCommit.slice(
+                0,
+                7
+            )}\`\nLatest Version: \`${remoteCommit}\``
+        );
     } else {
-        embed.setDescription(`Hello droiders!\n\n**Please Update to Latest Version**\nLocal Version: \`${localCommit.slice(0, 7)}\`\nLatest Version: \`${remoteCommit}\``);
+        embed.setDescription(
+            `Hello droiders!\n\n**Please Update to Latest Version**\nLocal Version: \`${localCommit.slice(
+                0,
+                7
+            )}\`\nLatest Version: \`${remoteCommit}\``
+        );
     }
 
-    const channelId = '620122073074892811';
+    const channelId = "620122073074892811";
 
     const channel = client.channels.cache.get(channelId);
     if (channel) {
@@ -79,23 +90,26 @@ async function setupEmbed() {
 
 let guildIds = [];
 
-// "ready" event listener updates bot activity
+// i did not vibe code this part
 client.on("ready", async () => {
     logger.info(`Logged in as ${client.user.tag}!`);
 
-    // Call setupEmbed (existing functionality)
+    // hi hello
     await setupEmbed();
 
-    // Fetch and log connected guilds
-    guildIds = await client.guilds.cache.map(guild => guild.id);
+    // i also did not vibe code this part
+    guildIds = await client.guilds.cache.map((guild) => guild.id);
     logger.info(`Connected to guilds: ${guildIds}`);
     await deployCommands(client, guildIds);
+
+    // where are you?
+    await updateSensorActivity(client);
 
     // Start updating the sensor data in activity every 30 seconds
     setInterval(() => updateSensorActivity(client), 30000); // 30-second interval
 });
 
-// Login with the bot token
+// Login
 client.login(token);
 
 module.exports = { guildIds, client };
